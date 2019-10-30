442cf26c1a6ae5786d55e4f5b87ef4c106003fcf
System Administrator <admin@example.com>
2019-10-30 13:20:10 -0400

    Changes by 'System Administrator' on 'CICD DevOps - 1.0.0' -- Build #1

---
+++ sn/CICD DevOps/Dictionary Entry/x_308211_cicd_devo_cicd_devops_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : CICD DevOps
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-30 17:18:50
+ * Created By  : admin
+ * Updated On  : 2019-10-30 17:18:50
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=436332f5dbf00010343f58b3ca961970
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
